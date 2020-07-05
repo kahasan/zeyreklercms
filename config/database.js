712a83d358df32d2f433e77d6b8e85a9bbd763fb
kahasan <kahraman.h@outlook.com>
2020-07-05 19:55:39 +0300

    Add database uri

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
+        uri: "mongodb+srv://zeyrekler:zeyrek43@cluster0.ujtdi.mongodb.net/zeyreklerfiyatlist?retryWrites=true&w=majority"
         host: env('DATABASE_HOST', '127.0.0.1'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
